d88a36335a5f9934fe2936aa875689b476973b41
Ilya Shkvalniy <ilyashkvalnij@gmail.com>
2022-01-18 14:44:46 +0300

    q

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 module.exports = function toReadable (num) {
-    const lowNums = ['',' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine',
-    ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen',
-    ' seventeen', ' eighteen', ' nineteen'];
-    const hightNums = ['','',' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty',
-    ' ninety']; 
+    const lowNums = ['',' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine',' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen',' seventeen', ' eighteen', ' nineteen'];
+    const hightNums = ['','',' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty',' ninety']; 
     const hundred = ' hundred';
 
      if(num === 0){
@@ -13,10 +10,8 @@ module.exports = function toReadable (num) {
     } else if ( num < 100) {
         return hightNums[parseInt(num.toString()[0])].substring(1)  + lowNums[parseInt(num.toString()[1])]
     } else if(num > 100 && parseInt(num.toString().substring(1)) < 20) {
-        console.log(lowNums[parseInt(num.toString().substring(1))])
         return  lowNums[parseInt(num.toString()[0])].substring(1) + hundred+ lowNums[parseInt(num.toString().substring(1))]
     } else {
         return  lowNums[parseInt(num.toString()[0])].substring(1) + hundred + hightNums[parseInt(num.toString()[1])] + lowNums[parseInt(num.toString()[2])]
     }
-
 }
